95f192c489b238d1c0b60e6a7a2240b21db8da70
pazzle1230 <pazzle1230@gmail.com>
2019-10-08 00:09:42 +0900

    add table

--- generate.py
+++ generate.py
@@ -5,14 +5,24 @@ import subprocess
 
 
 def get_option():
-    argparser = ArgumentParser(description='指定したテンプレートTeXファイルの"%%%INSERT HERE%%%"部分に別ファイルで指定したライブラリファイルを挿入する。')
-    argparser.add_argument('-t', '--template', default='template.tex',
+    argparser = ArgumentParser(
+        description=
+        '指定したテンプレートTeXファイルの"%%%INSERT HERE%%%"部分に別ファイルで指定したライブラリファイルを挿入する。')
+    argparser.add_argument('-t',
+                           '--template',
+                           default='template.tex',
                            help='テンプレートとなるTeXファイル')
-    argparser.add_argument('-l', '--library', default='library.toml',
+    argparser.add_argument('-l',
+                           '--library',
+                           default='library.toml',
                            help='使いたいライブラリのファイルをまとめたtomlファイル')
-    argparser.add_argument('-o', '--output', default='library.tex',
+    argparser.add_argument('-o',
+                           '--output',
+                           default='library.tex',
                            help='出力するファイルの名前')
-    argparser.add_argument('-p', '--pdf', help="コンパイルしてPDFファイルを作成する",
+    argparser.add_argument('-p',
+                           '--pdf',
+                           help="コンパイルしてPDFファイルを作成する",
                            action="store_true")
 
     return argparser.parse_args()
@@ -23,10 +33,10 @@ title_style = '\\noindent \\small\n'
 
 
 def write_lib_file(fp, name, path):
-    fp.write(title_style)
+    # fp.write(title_style)
     name = name.replace('_', '\_')
-    fp.write(name+'\n')
-    fp.write('\\myMintedfile{'+path+'}\n')
+    fp.write('\\subsection{' + name + '}\n')
+    fp.write('\\myMintedfile{' + path + '}\n')
 
 
 if __name__ == '__main__':
@@ -41,13 +51,14 @@ if __name__ == '__main__':
                 continue
 
             for libs in dict_toml['library']:
-                wf.write(category_style)
-                wf.write(libs['category']+'\\\\\n')
+                # wf.write(category_style)
+                wf.write('\\section{' + libs['category'] + '}\n')
 
                 if 'file' in libs:
                     for lib in libs['file']:
                         if not os.path.isfile(lib['path']):
-                            print("Failed to find the file: {}\n    name: {}".format(lib['path'], lib['name']))
+                            print("Failed to find the file: {}\n    name: {}".
+                                  format(lib['path'], lib['name']))
                             continue
                         path = os.path.abspath(lib['path'])
                         write_lib_file(wf, lib['name'], path)
@@ -58,15 +69,25 @@ if __name__ == '__main__':
                             continue
 
                         for lib in os.listdir(dir['path']):
-                            if not os.path.isfile(os.path.join(dir['path'], lib)):
+                            if not os.path.isfile(
+                                    os.path.join(dir['path'], lib)):
                                 continue
                             if not os.path.splitext(lib)[1] == '.cpp':
                                 continue
-                            path = os.path.abspath(os.path.join(dir['path'], lib))
-                            write_lib_file(wf, dir['name']+lib, path)
-    
+                            path = os.path.abspath(
+                                os.path.join(dir['path'], lib))
+                            write_lib_file(wf, dir['name'] + lib, path)
+
     if args.pdf:
         filename = args.output.split('.')[0]
-        subprocess.call(['platex', '-kanji=utf8', '-file-line-error', '-shell-escape', filename+'.tex'])
-        subprocess.call(['dvipdfmx', filename+'.dvi'])
-        subprocess.call(['rm', filename+'.dvi', filename+'.log', filename+'.aux'])
+        subprocess.call([
+            'platex', '-kanji=utf8', '-file-line-error', '-shell-escape',
+            filename + '.tex'
+        ])
+        subprocess.call([
+            'platex', '-kanji=utf8', '-file-line-error', '-shell-escape',
+            filename + '.tex'
+        ])
+        subprocess.call(['dvipdfmx', filename + '.dvi'])
+        subprocess.call(
+            ['rm', filename + '.dvi', filename + '.log', filename + '.aux'])
